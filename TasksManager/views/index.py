fd2edddc793de6ec9ada0ab453709edc20137d4c
Mohammad Moghaddam <moghaddam22@gmail.com>
2015-04-13 20:08:46 -0700

    minor edit

--- TasksManager/views/index.py
+++ TasksManager/views/index.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 #View for index page.
 def page(request):
-    new_project = Project(title = "Tasks Manager with Django", 
-            description = "Django project to getting start with Django
-            easily.", client_name = "Me")
+    new_project = Project(title = "Tasks Manager with Django", description = "Django project to getting start with Django easily.", client_name = "Me")
     new_project.save()
-    return render(request, 'en/public/index.html', {'action': 'Save dates
-    of model'})
+    return render(request, 'en/public/index.html', {'action': 'Save dates of model'})
