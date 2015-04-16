c49ae155b03819e3266e7b86e02dc9b1faa9246e
Mohammad Moghaddam <moghaddam22@gmail.com>
2015-04-16 16:59:36 -0700

    edit index.py

--- TasksManager/views/index.py
+++ TasksManager/views/index.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from TasksManager.models import Project
+
 #View for index page.
 def page(request):
     new_project = Project(title = "Tasks Manager with Django", description = "Django project to getting start with Django easily.", client_name = "Me")
     new_project.save()
+<<<<<<< Updated upstream
     return render(request, 'en/public/index.html', {'action': 'Save dates of model'})
+=======
+    return render(request, 'en/public/index.html', {'action': 'Save datas of model'})
+>>>>>>> Stashed changes
